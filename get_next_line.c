d4f1e57a6ce65f428e52bdcec61ddd3c9f1b2007
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-09 00:14:59 +0200

    gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,3 +12,68 @@
 
 #include "get_next_line.h"
 
+char	*ft_temp(int fd, char *buffer, char *stash)
+{
+	int		i;
+	char	*temp;
+
+	i = 1;
+	while (i != '\0')
+	{
+		i = read(fd, buffer, BUFFER_SIZE);
+		if (i == -1)
+			return (0);
+		else if (i == 0)
+			break ;
+		buffer[i] = '\0';
+		if (!stash)
+			stash = ft_strdup("");
+		temp = stash;
+		stash = ft_strjoin(temp, buffer);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (stash);
+}
+
+char	*ft_get_line(char *line)
+{
+	char	*stash;
+	size_t	i;
+
+	i = 0;
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	if (line[i] == '\0' || line[1] == '\0')
+		return (NULL);
+	stash = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (*stash == '\0')
+	{
+		free(stash);
+		stash = NULL;
+	}
+	line[i + 1] = '\0';
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buffer;
+	static char	*stash;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	line = ft_temp(fd, buffer, stash);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
+		return (NULL);
+	stash = ft_get_line(line);
+	return (line);
+}
