5f31472b2dca18584acbdb8e731a2507f2f57c1f
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-12 12:01:51 +0200

    gnl_bonus.h

--- gnl_bonus/get_next_line_bonus.h
+++ gnl_bonus/get_next_line_bonus.h
@@ -5,9 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/09 18:04:25 by baalbade          #+#    #+#             */
-/*   Updated: 2022/10/09 18:04:27 by baalbade         ###   ########.fr       */
+/*   Created: 2022/10/12 12:01:26 by baalbade          #+#    #+#             */
+/*   Updated: 2022/10/12 12:01:29 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*get_next_line(int fd);
+
+#endif
