4e5de9e20e6e55b2b1d12305424d279ad10f8c4c
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-13 09:50:52 +0200

    gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -15,40 +15,24 @@
 // Read fd and stash characters read up to '\n' and save it to line
 char	*ft_read_file(int fd, char *buffer, char *stash)
 {
-	// i = tot count of element read
 	int		i;
-	// *temp arr to hold stash content
 	char	*temp;
 
-	// initialize i to 1 in order to start the loop
 	i = 1;
 	while (i != '\0')
 	{
-		// read file
 		i = read(fd, buffer, BUFFER_SIZE);
-
-		// and make sure there is something in it
 		if (i == -1)
 			return (0);
 		else if (i == 0)
 			break ;
-
-		// after read() initialize last element of buffer to '\0' 
 		buffer[i] = '\0';
-
-		// if stash has no content initialize it to empty str in order to strjoin
 		if (!stash)
 			stash = ft_strdup("");
-
-		// otherwise assign stash to temp
 		temp = stash;
-		// and reassign stash with join of temp + buffer read for every loop iteration
 		stash = ft_strjoin(temp, buffer);
-		// then free temp for following iteration
 		free(temp);
 		temp = NULL;
-		
-		// if '\n' is read, break
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
@@ -59,21 +43,14 @@ char	*ft_read_file(int fd, char *buffer, char *stash)
 // and extract and save the new characters to stash
 char	*ft_extract_from_line(char *line)
 {
-	// array that will hold the extraction from line
 	char	*stash;
-	// start = starting point for the extraction
 	size_t	start;
 
 	start = 0;
-	// iterate through line
 	while (line[start] != '\n' && line[start] != '\0')
 		start++;
-
-	// check if end of file is reached
 	if (line[start] == '\0' || line[1] == '\0')
 		return (NULL);
-
-	// copy to stash from start up to (line length - start) from line
 	stash = ft_substr(line, start + 1, ft_strlen(line) - start);
 	if (*stash == '\0')
 	{
